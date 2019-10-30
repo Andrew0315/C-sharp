6c24be1483c0045e7aaa0e30155f996570f7cb55
Andrew0315 <1836790069@qq.com>
2019-10-31 01:09:36 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int n;
+        n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("{1}", n, Program.F(n));
+        Console.ReadKey(true);
+    }
+    static ulong F(int n)
+    {
+        if (1 == n)
+            return 1;
+        else
+        {
+            ulong t, t1 = 0, t2 = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                t = t2;
+                t2 += t1;
+            }
+            return t2;
+        }
+    }
+}
